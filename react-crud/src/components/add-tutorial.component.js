bc02b7f143653da1588df4f8c6167a3493be69c7
Apex Limitless Gaming <102706261+JohnnyBravo-99@users.noreply.github.com>
2024-09-15 05:51:41 -0400

    react-crud proper?

---
+++ react-crud/src/components/add-tutorial.component.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import TutorialDataService from "../services/tutorial.service";
+
+export default class AddTutorial extends component {
+    constructor(props) {
+        super(props)
+        this.onChangeTitle = this.onChangeTitle.bind(this);
+        this.onChangeDescription = this.onChangeDescription.bind(this);
+        this.saveTutorial = this.saveTutorial.bind(this);
+        this.newTutorial = this.newTutorial.bind(this);
+
+        this.state = {
+            id: null,
+            title: "",
+            description: "",
+            published: false,
+
+            submitted: false
+        };
+    }
+
+    onChangeTitle(e) {
+        this.setState({ title: e.target.value });
+    }
+
+    onChangeDescription(e) {
+        this.setState({ description: e.target.value });
+    }
+
+    TutorialDataService.create(data)
+    .then(response => {
+        this.setState({
+            id: response.data.id,
+            title: response.data.title,
+            description: response.data.description,
+            published: response.data.published,
+
+            submitted: true
+        });
+console.log(response.data);
+})
+
+.catch (e => {
+    console.log(e);
+});
+}
+
+render(){
+
+}
+}
